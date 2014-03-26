d1e23991f68ff8cfeba6a8f096f73de21472bbf7
Kevin Leake <kevinleake01@gmail.com>
2014-03-26 17:55:18 +0000

    Adding C++ editions

---
+++ 06-gendata/tpl-gendata-0001.cpp
@@ -0,0 +1,63 @@
+/*
+####################################
+#
+# -- TEXTPATGEN GENDATA TEMPLATE --
+#
+# Example usage:
+# tpl-gendata-0001.cpp 255 16 255 100
+#
+####################################
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[]);
+
+long int num, finish;
+long int width, size;
+int randnum1, randnum2, randnum3, randnum4;
+unsigned int randnum5;
+
+int main(int argc, char *argv[])
+{
+  finish=std::atol(argv[1]);
+  width=std::atol(argv[2]);
+  randnum3=std::atoi(argv[3]);
+  randnum4=std::atoi(argv[4]);
+
+  std::srand(randnum4);  /* Generate random number seed */
+
+  std::printf("####################################\n");
+  std::printf("#\n");
+  std::printf("# -- TEXTPATGEN GENERATED RANDOM DATA --\n");
+  std::printf("#\n");
+  std::printf("# -- A generated random data file created from:\n");
+  std::printf("# -- tpl-gendata-0001.cpp\n");
+  std::printf("#\n");
+  std::printf("# Total number: %ld\n",finish);
+  std::printf("# Number in a line: %ld\n",width);
+  std::printf("# Highest printable number: %d\n",randnum3);
+  std::printf("# Random number seed: %d\n",randnum4);
+  std::printf("#\n");
+  std::printf("####################################\n");
+
+  for (num=1; num<=finish; num++)
+  {
+    for (size=0; size<width-1; size++)
+    {
+      if (num == finish) break;
+      randnum2=std::rand();  /* Put random number into randnum2 */
+      randnum5=(unsigned int)(randnum2%randnum3); /* Generate the highest printable number */
+      std::printf("%02x", randnum5);  /* Print this number */
+      num++;
+    }
+    randnum2=std::rand();  /* Put random number into randnum2 */
+    randnum5=(unsigned int)(randnum2%randnum3); /* Generate the highest printable number */
+    std::printf("%02x\n", randnum5);  /* Print this number */
+  }
+}
+
---
+++ 06-gendata/tpl-gendata-0005.cpp
@@ -0,0 +1,63 @@
+/*
+####################################
+#
+# -- TEXTPATGEN GENDATA TEMPLATE --
+#
+# Example usage:
+# tpl-gendata-0005.cpp 100 10 32767 100
+#
+####################################
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[]);
+
+long int num, finish;
+long int width, size;
+int randnum1, randnum2, randnum3, randnum4;
+unsigned int randnum5;
+
+int main(int argc, char *argv[])
+{
+  finish=std::atol(argv[1]);
+  width=std::atol(argv[2]);
+  randnum3=std::atoi(argv[3]);
+  randnum4=std::atoi(argv[4]);
+
+  std::srand(randnum4);  /* Generate random number seed */
+
+  std::printf("####################################\n");
+  std::printf("#\n");
+  std::printf("# -- TEXTPATGEN GENERATED RANDOM DATA --\n");
+  std::printf("#\n");
+  std::printf("# -- A generated random data file created from:\n");
+  std::printf("# -- tpl-gendata-0005.cpp\n");
+  std::printf("#\n");
+  std::printf("# Total number: %ld\n",finish);
+  std::printf("# Number in a line: %ld\n",width);
+  std::printf("# Highest printable number: %d\n",randnum3);
+  std::printf("# Random number seed: %d\n",randnum4);
+  std::printf("#\n");
+  std::printf("####################################\n");
+
+  for (num=1; num<=finish; num++)
+  {
+    for (size=0; size<width-1; size++)
+    {
+      if (num == finish) break;
+      randnum2=std::rand();  /* Put random number into randnum2 */
+      randnum5=(unsigned int)(randnum2%randnum3); /* Generate the highest printable number */
+      std::printf("%05u ", randnum5);  /* Print this number */
+      num++;
+    }
+    randnum2=std::rand();  /* Put random number into randnum2 */
+    randnum5=(unsigned int)(randnum2%randnum3); /* Generate the highest printable number */
+    std::printf("%05u\n", randnum5);  /* Print this number */
+  }
+}
+
