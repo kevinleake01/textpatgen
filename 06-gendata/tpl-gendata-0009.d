321a1ba97dcc3eafd23a2da7747ec639ca8be783
Kevin Leake <kevinleake01@gmail.com>
2014-03-27 20:39:40 +0000

    Updates

---
+++ 06-gendata/tpl-gendata-0009.d
@@ -0,0 +1,57 @@
+/*
+####################################
+#
+# -- TEXTPATGEN GENDATA TEMPLATE --
+#
+# Example usage:
+# tpl-gendata-0009.d 100 10 0 32767 100
+#
+####################################
+*/
+
+module tpl_gendata_0009;
+import std.stdio, std.conv, std.string, std.format, std.random;
+
+int num, finish, width, size, randnum, randmin, randmax, randseed;
+
+void main(string[] args)
+{
+  finish=to!int(args[1]);
+  width=to!int(args[2]);
+  randmin=to!int(args[3]);
+  randmax=to!int(args[4]);
+  randseed=to!int(args[5]);
+
+  auto gen=Random(randseed);
+
+  writef("####################################\n");
+  writef("#\n");
+  writef("# -- TEXTPATGEN GENERATED RANDOM DATA --\n");
+  writef("#\n");
+  writef("# -- A generated random data file created from:\n");
+  writef("# -- tpl-gendata-0009.d\n");
+  writef("#\n");
+  writef("# Total number: %d\n",finish);
+  writef("# Number in a line: %d\n",width);
+  writef("# Lowest printable number: %d\n",randmin);
+  writef("# Highest printable number: %d\n",randmax);
+  writef("# Random number seed: %d\n",randseed);
+  writef("#\n");
+  writef("####################################\n");
+
+  for (num=1; num<=finish; num++)
+  {
+    for (size=0; size<width-1; size++)
+    {
+      if (num == finish) break;
+      randnum=uniform(randmin, randmax, gen);  /* Get the random number */
+      printf("%05u ", randnum);  /* Print this number */
+      num++;
+    }
+    randnum=uniform(randmin, randmax, gen);  /* Get the random number */
+    printf("%05u\n", randnum);  /* Print this number */
+  }
+}
+
+
+
