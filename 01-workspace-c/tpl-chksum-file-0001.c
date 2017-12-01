08d9aed48f517ba3bb7d60467ad2f31258fc3073
Kevin Leake <kevinleake01@gmail.com>
2017-12-01 22:47:15 +0000

    Add files via upload

---
+++ 01-workspace-c/tpl-chksum-file-0001.c
@@ -0,0 +1,75 @@
+/*
+####################################
+#
+# -- TEXTPATGEN TEMPLATE --
+#
+# Example usage:
+# tpl-chksum-file-0001.c 255 16 "+"
+#
+# The text is written to a timestamped file.
+#
+####################################
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[]);
+
+long int num, finish;
+long int width, size;
+char message[1000];
+FILE *fp;
+
+/* Timestamp info */
+time_t timer;
+char buffer1[32], buffer2[32];
+struct tm* tm_info;
+
+int main(int argc, char *argv[])
+{
+  finish=atol(argv[1]);
+  width=atol(argv[2]);
+  strcpy(message, argv[3]);
+
+  time(&timer);  /* get the time */
+  tm_info=localtime(&timer);
+
+  strftime(buffer1, 32, "%Y-%m-%dT%H:%M:%S%z", tm_info); /* Timestamp to print */
+  strftime(buffer2, 32, "00_%Y%m%d_%H%M%S.txt", tm_info); /* Timestamp for filename */
+
+  fp=fopen(buffer2,"w");  /* Open file for writing */
+  fprintf(fp,"####################################\n");
+  fprintf(fp,"#\n");
+  fprintf(fp,"# -- TEXTPATGEN GENERATED FILE --\n");
+  fprintf(fp,"#\n");
+  fprintf(fp,"# -- A generated text file created from:\n");
+  fprintf(fp,"# -- tpl-chksum-file-0001.c\n");
+  fprintf(fp,"#\n");
+  fprintf(fp,"# -- This file can be very useful in\n");
+  fprintf(fp,"# -- transmission tests.\n");
+  fprintf(fp,"#\n");
+  fprintf(fp,"# -- Created: %s\n", buffer1);
+  fprintf(fp,"#\n");
+  fprintf(fp,"# Total number: %ld\n",finish);
+  fprintf(fp,"# Number in a line: %ld\n",width);
+  fprintf(fp,"# Text: \"%s\"\n",message);
+  fprintf(fp,"#\n");
+  fprintf(fp,"####################################\n");
+
+  for (num=1; num<=finish; num++)
+  {
+    for (size=0; size<width-1; size++)
+    {
+      if (num == finish) break;
+      fprintf(fp,"%s", message);
+      num++;
+    }
+    fprintf(fp,"%s\n", message);
+  }
+  fclose(fp); /* Close the file */
+  return 0;
+}
+
