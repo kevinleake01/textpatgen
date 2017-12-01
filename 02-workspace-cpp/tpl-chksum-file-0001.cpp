51ef5fbf7da13cdb77325069137a71116a47560e
Kevin Leake <kevinleake01@gmail.com>
2017-12-01 22:47:42 +0000

    Add files via upload

---
+++ 02-workspace-cpp/tpl-chksum-file-0001.cpp
@@ -0,0 +1,78 @@
+/*
+####################################
+#
+# -- TEXTPATGEN TEMPLATE --
+#
+# Example usage:
+# tpl-chksum-file-0001.cpp 255 16 "+"
+#
+# The text is written to a timestamped file.
+#
+####################################
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char *argv[]);
+
+long int num, finish;
+long int width, size;
+char message[1000];
+FILE *fp;
+
+/* Timestamp info */
+time_t timer;
+char buffer1[32], buffer2[32];
+struct tm* tm_info;
+
+int main(int argc, char *argv[])
+{
+  finish=std::atol(argv[1]);
+  width=std::atol(argv[2]);
+  std::strcpy(message, argv[3]);
+
+  std::time(&timer);  /* get the time */
+  tm_info=std::localtime(&timer);
+
+  std::strftime(buffer1, 32, "%Y-%m-%dT%H:%M:%S%z", tm_info); /* Timestamp to print */
+  std::strftime(buffer2, 32, "00_%Y%m%d_%H%M%S.txt", tm_info); /* Timestamp for filename */
+
+  fp=std::fopen(buffer2,"w");  /* Open file for writing */
+  std::fprintf(fp,"####################################\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- TEXTPATGEN GENERATED FILE --\n");
+  std::fprintf(fp,"# -- C++ VERSION --\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- A generated text file created from:\n");
+  std::fprintf(fp,"# -- tpl-chksum-file-0001.cpp\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- This file can be very useful in\n");
+  std::fprintf(fp,"# -- transmission tests.\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- Created: %s\n", buffer1);
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# Total number: %ld\n",finish);
+  std::fprintf(fp,"# Number in a line: %ld\n",width);
+  std::fprintf(fp,"# Text: \"%s\"\n",message);
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"####################################\n");
+
+  for (num=1; num<=finish; num++)
+  {
+    for (size=0; size<width-1; size++)
+    {
+      if (num == finish) break;
+      std::fprintf(fp,"%s", message);
+      num++;
+    }
+    std::fprintf(fp,"%s\n", message);
+  }
+  std::fclose(fp); /* Close the file */
+  return 0;
+}
+
