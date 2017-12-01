a7a8745e7e6b650af2a1d578055bae0f462f375f
Kevin Leake <kevinleake01@gmail.com>
2017-12-01 20:00:22 +0000

    Add files via upload

---
+++ 02-workspace-cpp/tpl-text-file-0001.cpp
@@ -0,0 +1,66 @@
+/*
+####################################
+#
+# -- TEXTPATGEN TEMPLATE --
+#
+# Example usage:
+# tpl-text-file-0001.cpp 0 20 "The_Quick_Brown_Fox_Jumps_over_the_Lazy_Dog"
+#
+# The text is written to a timestamped file.
+#
+####################################
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char *argv[]);
+
+long int num, start, finish;
+char message[1000];
+FILE *fp;
+
+/* Timestamp info */
+time_t timer;
+char buffer1[32], buffer2[32];
+struct tm* tm_info;
+
+int main(int argc, char *argv[])
+{
+  start=std::atol(argv[1]);
+  finish=std::atol(argv[2]);
+  std::strcpy(message, argv[3]);
+
+  std::time(&timer);  /* get the time */
+  tm_info=std::localtime(&timer);
+
+  std::strftime(buffer1, 32, "%Y-%m-%dT%H:%M:%S%z", tm_info); /* Timestamp to print */
+  std::strftime(buffer2, 32, "00_%Y%m%d_%H%M%S.txt", tm_info); /* Timestamp for filename */
+
+  fp=std::fopen(buffer2,"w");  /* Open file for writing */
+  std::fprintf(fp,"####################################\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- TEXTPATGEN GENERATED FILE --\n");
+  std::fprintf(fp,"# -- C++ VERSION --\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- tpl-text-file-0001.cpp --\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- %s --\n", buffer1);
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# Start number: %ld ( 0x%lX Hex, 0%lo Octal )\n",start,start,start);
+  std::fprintf(fp,"# Finish number: %ld ( 0x%lX Hex, 0%lo Octal )\n",finish,finish,finish);
+  std::fprintf(fp,"# Message text: \"%s\"\n",message);
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"####################################\n");
+  for (num=start; num<=finish; num++)
+  {
+    std::fprintf(fp,"X%04lX:%s:D%04ld\n", num, message, num);
+  }
+  std::fprintf(fp,"# -- End of file.\n");
+  return 0;
+}
+
