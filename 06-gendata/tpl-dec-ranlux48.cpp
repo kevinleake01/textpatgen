4c7b4c0729bc72be2d24d76e03f040869aaf641e
kevinleake01 <kevinleake01@gmail.com>
2024-05-21 12:49:24 +0000

    Various updates

---
+++ 06-gendata/tpl-dec-ranlux48.cpp
@@ -0,0 +1,84 @@
+/*
+####################################
+#
+# -- TEXTPATGEN GENDATA TEMPLATE --
+#
+# Example usage:
+# tpl-dec-ranlux48.cpp 100 10 0 99999 100
+#
+# The text is written to a timestamped file.
+#
+####################################
+*/
+
+// ----- Written in ISO/IEC 14882:2011 standard C++
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <random>
+
+long int num, finish, width, size, randnum, randmin, randmax, randseed;
+FILE *fp;
+
+/* Timestamp info */
+time_t timer;
+char buffer1[32], buffer2[32];
+struct tm* tm_info;
+
+int main(int argc, char* argv[])
+{
+  finish=std::strtol(argv[1],0,10);
+  width=std::strtol(argv[2],0,10);
+  randmin=std::strtol(argv[3],0,10);
+  randmax=std::strtol(argv[4],0,10);
+  randseed=std::strtol(argv[5],0,10);
+
+  /* Random number generator, requires C++11 or later */
+  std::ranlux48 generator(randseed);
+  std::uniform_int_distribution<long> distribution(randmin,randmax);
+
+  std::time(&timer);  /* get the time */
+  tm_info=std::localtime(&timer);
+
+  std::strftime(buffer1, 32, "%Y-%m-%dT%H:%M:%S%z", tm_info); /* Timestamp to print */
+  std::strftime(buffer2, 32, "00_%Y%m%d_%H%M%S.txt", tm_info); /* Timestamp for filename */
+
+  fp=std::fopen(buffer2,"w");  /* Open file for writing */
+  std::fprintf(fp,"####################################\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- TEXTPATGEN GENERATED RANDOM DATA --\n");
+  std::fprintf(fp,"# -- C++ VERSION --\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- A generated random data file created from:\n");
+  std::fprintf(fp,"# -- tpl-dec-ranlux48.cpp\n");
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# -- %s --\n", buffer1);
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"# Total number: %ld\n",finish);
+  std::fprintf(fp,"# Number in a line: %ld\n",width);
+  std::fprintf(fp,"# Lowest printable number: %ld\n",randmin);
+  std::fprintf(fp,"# Highest printable number: %ld\n",randmax);
+  std::fprintf(fp,"# Random number seed: %ld\n",randseed);
+  std::fprintf(fp,"#\n");
+  std::fprintf(fp,"####################################\n");
+
+  for (num=1; num<=finish; num++)
+  {
+    for (size=0; size<width-1; size++)
+    {
+      if (num == finish) break;
+      randnum=distribution(generator);  /* Get the random number */
+      std::fprintf(fp,"%05ld ", randnum);  /* Print this number */
+      num++;
+    }
+    randnum=distribution(generator);  /* Get the random number */
+    std::fprintf(fp,"%05ld\n", randnum);  /* Print this number */
+  }
+  fclose(fp);
+  return 0;
+}
+
+
+
---
+++ 14-cpp-ide/tpl-gendata-ide-0006.cpp
@@ -0,0 +1,63 @@
+/*
+####################################
+#
+# -- TEXTPATGEN TEMPLATE --
+#
+# Users can change the output by editing
+# this file directly.
+#
+####################################
+*/
+
+// ----- Written in ISO/IEC 14882:2011 standard C++
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <random>
+
+long int num, finish, width, size, randnum, randmin, randmax, randseed;
+
+int main(int argc, char* argv[])
+{
+  finish=256;
+  width=16;
+  randmin=0;
+  randmax=255;
+  randseed=100;
+
+  std::default_random_engine generator(randseed);
+  std::uniform_int_distribution<long> distribution(randmin,randmax);
+
+  std::printf("####################################\n");
+  std::printf("#\n");
+  std::printf("# -- TEXTPATGEN GENERATED RANDOM DATA --\n");
+  std::printf("#\n");
+  std::printf("# -- A generated random data file created from:\n");
+  std::printf("# -- tpl-gendata-ide-0006.cpp\n");
+  std::printf("#\n");
+  std::printf("# Total number: %ld\n",finish);
+  std::printf("# Number in a line: %ld\n",width);
+  std::printf("# Lowest printable number: %ld\n",randmin);
+  std::printf("# Highest printable number: %ld\n",randmax);
+  std::printf("# Random number seed: %ld\n",randseed);
+  std::printf("#\n");
+  std::printf("####################################\n");
+
+  for (num=1; num<=finish; num++)
+  {
+    for (size=0; size<width-1; size++)
+    {
+      if (num == finish) break;
+      randnum=distribution(generator);  /* Get the random number */
+      std::printf("%02lx ", randnum);  /* Print this number */
+      num++;
+    }
+    randnum=distribution(generator);  /* Get the random number */
+    std::printf("%02lx\n", randnum);  /* Print this number */
+  }
+  return 0;
+}
+
+
+
