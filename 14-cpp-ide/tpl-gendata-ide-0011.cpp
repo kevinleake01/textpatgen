356de8c57a2991fdbdad8b71ecad85e45a6ca416
Kevin Leake <kevinleake01@gmail.com>
2026-02-15 19:29:29 +0000

    Updated 14-cpp-ide

---
+++ 14-cpp-ide/tpl-gendata-ide-0011.cpp
@@ -0,0 +1,78 @@
+/*
+####################################
+#
+# -- TEXTPATGEN TEMPLATE --
+#
+# Users can change the output by editing
+# this file directly.
+#
+####################################
+*/
+
+// ----- Written in ISO/IEC 14882:2011 standard C++
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <random>
+
+long int num, finish, width, size, randnum, randmin, randmax, randseed;
+
+/* Timestamp info */
+time_t timer;
+char buffer1[32], buffer2[32];
+struct tm* tm_info;
+
+int main(int argc, char* argv[])
+{
+  finish=100;
+  width=10;
+  randmin=0;
+  randmax=65535;
+  randseed=100;
+
+  std::default_random_engine generator(randseed);
+  std::uniform_int_distribution<long> distribution(randmin,randmax);
+
+  std::time(&timer);  /* get the time */
+  tm_info=std::localtime(&timer);
+
+  std::strftime(buffer1, 32, "%Y-%m-%dT%H:%M:%S%z", tm_info); /* ISO-8601 Timestamp */
+  std::strftime(buffer2, 32, "%s", tm_info); /* Epoch Timestamp */
+
+  std::printf("####################################\n");
+  std::printf("#\n");
+  std::printf("# -- TEXTPATGEN GENERATED RANDOM DATA --\n");
+  std::printf("#\n");
+  std::printf("# -- A generated random data file created from:\n");
+  std::printf("# -- tpl-gendata-ide-0011.cpp\n");
+  std::printf("#\n");
+  std::printf("# -- Created: %s\n", buffer1);
+  std::printf("# -- Epoch Time: %s\n", buffer2);
+  std::printf("#\n");
+  std::printf("# Total number: %ld\n",finish);
+  std::printf("# Number in a line: %ld\n",width);
+  std::printf("# Lowest printable number: %ld\n",randmin);
+  std::printf("# Highest printable number: %ld\n",randmax);
+  std::printf("# Random number seed: %ld\n",randseed);
+  std::printf("#\n");
+  std::printf("####################################\n");
+
+  for (num=1; num<=finish; num++)
+  {
+    for (size=0; size<width-1; size++)
+    {
+      if (num == finish) break;
+      randnum=distribution(generator);  /* Get the random number */
+      std::printf("D-%05ld ", randnum);  /* Print this number */
+      num++;
+    }
+    randnum=distribution(generator);  /* Get the random number */
+    std::printf("D-%05ld\n", randnum);  /* Print this number */
+  }
+  return 0;
+}
+
+
+
